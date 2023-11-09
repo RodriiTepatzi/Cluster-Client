b5c414e1adceace914cbee2bfe58ba72ffc482ea
Omsod0404 <garciaomarucho@gmail.com>
2023-11-08 22:48:53 -0600

    UC-21 Dialog para seleccionar el video (falta de chequeo por disponibilidad de los otros codigo)

--- Core/Core.cs
+++ Core/Core.cs
@@ -28,29 +28,37 @@ namespace Cluster_Client.Core
 
         public Connection LocalConnection { get { return _localConnection; } }
         public bool IsConnected { get; private set; }
+        public bool IsVideoLoaded { get; private set; }
         public Status ServerDisponibility { get; private set; }
         public int ClientsBefore { get; private set; }
+        public byte[] LocalVideo { get; private set; }
 
         //Eventos para actualizar la interfaz
         public event EventHandler<ConnectedStatusEventArgs>? ConnectedStatusEvent;
-        public event EventHandler<MessageReceivedEventArgs>? MessageReceivedEvent;
+        public event EventHandler<VideoLoadedEventArgs>? VideoLoadedEvent;
         public event EventHandler<ServerDisponibilityEventArgs>? ServerDisponibilityEvent;
         public event EventHandler<ClientsBeforeEventArgs>? ClientsBeforeEvent;
+        public event EventHandler<LocalVideoEventArgs>? LocalVideoEvent;
 
         private void OnStatusConnectedChanged(ConnectedStatusEventArgs e) => ConnectedStatusEvent?.Invoke(this, e);
-        private void OnMessageReceived(MessageReceivedEventArgs e) => MessageReceivedEvent?.Invoke(this, e);
+        private void OnVideoLoaded(VideoLoadedEventArgs e) => VideoLoadedEvent?.Invoke(this, e);
         private void OnServerDisponibility(ServerDisponibilityEventArgs e) => ServerDisponibilityEvent?.Invoke(this, e);
         private void OnClientsBefore(ClientsBeforeEventArgs e) => ClientsBeforeEvent?.Invoke(this, e);
+        private void OnLacalVideo(LocalVideoEventArgs e) => LocalVideoEvent?.Invoke(this, e);
 
         private void HandleConnectionStatus(bool value) => OnStatusConnectedChanged(new ConnectedStatusEventArgs(value));
-        private void HandleMessageReceived(string value) => OnMessageReceived(new MessageReceivedEventArgs(value));
+        private void HandleVideoLoaded(bool value) => OnVideoLoaded(new VideoLoadedEventArgs(value));
         private void HandleServerDisponibility(Status value) => OnServerDisponibility(new ServerDisponibilityEventArgs(value));
         private void HandleClientsBefore(int value) => OnClientsBefore(new ClientsBeforeEventArgs(value));
+        private void HandleLocalVideo(byte[] value) => OnLacalVideo(new LocalVideoEventArgs(value));
+
         private CoreHandler()
         {
             IsConnected = false;
             ServerDisponibility = Status.Waiting;
             ClientsBefore = 0;
+            IsVideoLoaded = false;
+            byte[] LocalVideo = new byte[0];
         }
 
         public static CoreHandler Instance
@@ -84,8 +92,6 @@ namespace Cluster_Client.Core
 
                 if (_client.Connected)
                 {
-                    IsConnected = true;
-                    HandleConnectionStatus(IsConnected);
                     _serverConnection.Stream = _client.GetStream();
                     _serverConnection.StreamWriter = new StreamWriter(_serverConnection.Stream);
                     _serverConnection.StreamReader = new StreamReader(_serverConnection.Stream);
@@ -107,6 +113,9 @@ namespace Cluster_Client.Core
                     _serverConnection.StreamWriter.WriteLine(json);
                     _serverConnection.StreamWriter.Flush();
 
+                    IsConnected = true;
+                    HandleConnectionStatus(IsConnected);
+
                     Thread thread = new Thread(ListenToServerAsync);
                     thread.Start();
                 }
@@ -121,17 +130,20 @@ namespace Cluster_Client.Core
         {
             while (_client.Connected)
             {
+
                 try
                 {
+
                     var dataFromClient = _serverConnection.StreamReader!.ReadLine();
                     var model = JsonConvert.DeserializeObject<Message>(dataFromClient!);
 
                     if (model!.Type == MessageType.Status)
                     {
+
                         var json = model.Content as string;
                         var content = JsonConvert.DeserializeObject<Status>(json!);
                         Status ServerDisponibility = content;
-                        
+
                         Application.Current.Dispatcher.Invoke(new Action(() =>
                         {
                             HandleServerDisponibility(ServerDisponibility);
@@ -139,7 +151,12 @@ namespace Cluster_Client.Core
                     }
                     if (model!.Type == MessageType.Turn)
                     {
-                        var x = 1;
+                        ClientsBefore = int.Parse(model!.Content! as string);
+
+                        Application.Current.Dispatcher.Invoke(new Action(() =>
+                        {
+                            HandleClientsBefore(ClientsBefore);
+                        }));
                     }
                 }
                 catch
@@ -164,6 +181,33 @@ namespace Cluster_Client.Core
             IsConnected = false;
             _client.Close();
         }
-    }
 
+        public void LoadVideo()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de video|*.avi;*.wmv;*.mp4;*.mkv;*.mpeg;*.flv;*.3gp;*.mov|Todos los archivos (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string path = openFileDialog.FileName;
+
+                if (!string.IsNullOrEmpty(path))
+                {
+                    byte[] video = File.ReadAllBytes(path);
+                    string videoName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                    string videoExtension = System.IO.Path.GetExtension(openFileDialog.FileName).TrimStart('.').ToLower();
+
+                    if (video.Length > 0)
+                    {
+                        IsVideoLoaded = true;
+                        Application.Current.Dispatcher.Invoke(new Action(() =>
+                        {
+                            HandleVideoLoaded(IsVideoLoaded);
+                            HandleLocalVideo(video);
+                        }));
+                    }
+
+                }
+            }
+        }
+    }
 }
--- ViewModel/VideoActionsViewModel.cs
+++ ViewModel/VideoActionsViewModel.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows;
 using System.Windows.Markup;
 using System.Net;
+using Cluster_Client.Model;
 
 namespace Cluster_Client.ViewModel
 {
@@ -19,13 +20,33 @@ namespace Cluster_Client.ViewModel
     {
         private CoreHandler _coreHandler;
         private ICommand _executeCloseWindowCommand;
+        private ICommand _executeLoadVideoCommand;
+        private ICommand _executePSLocalVideoCommand;
+
         private bool _isConnected;
+        private bool _isVideoLoaded;
+        private Status _serverDisponibility;
+        private int _clientsBefore;
+        private string _messagesPath;
+        private string _messages;
+        private Grid _allContent;
+        private UIElement _defaultContent;
+        private List<RowDefinition> _defaultRowDefinitions;
+        private byte[] _localVideo;
+        private bool _isLocalVideoStop;
 
         public ICommand ExecuteCloseWindowCommand
         {
             get { return _executeCloseWindowCommand; }
         }
-
+        public ICommand ExecuteLoadVideoCommand
+        { 
+            get { return _executeLoadVideoCommand; } 
+        }
+        public ICommand ExecutePSLocalVideoCommand
+        {
+            get { return _executePSLocalVideoCommand; }
+        }
         public bool IsConnected
         {
             get { return _isConnected; }
@@ -35,17 +56,197 @@ namespace Cluster_Client.ViewModel
                 OnPropertyChanged(nameof(IsConnected));
             }
         }
-
-        public VideoActionsViewModel()
+        public bool IsVideoLoaded
         {
+            get { return _isVideoLoaded; }
+            set
+            {
+                _isVideoLoaded = value;
+                OnPropertyChanged(nameof(IsVideoLoaded));
+            }
+        }
+        public Status ServerDisponibility
+        {
+            get { return _serverDisponibility; }
+            set
+            {
+                _serverDisponibility = value;
+                OnPropertyChanged(nameof(ServerDisponibility));
+            }
+        }
+        public int ClientsBefore
+        {
+            get { return _clientsBefore; }
+            set
+            {
+                _clientsBefore = value;
+                OnPropertyChanged(nameof(ClientsBefore));
+            }
+        }
+        public string MessagesPath
+        {
+            get { return _messagesPath; }
+            set
+            {
+                _messagesPath = value;
+                OnPropertyChanged(nameof(MessagesPath));
+            }
+        }
+        public string Messages
+        {
+            get { return _messages; }
+            set
+            {
+                _messages = value;
+                OnPropertyChanged(nameof(Messages));
+            }
+        }
+        public byte[] LocalVideo
+        {
+            get { return _localVideo; }
+            set
+            {
+                _localVideo = value;
+                OnPropertyChanged(nameof(LocalVideo));
+            }
+        }
+        public bool IsLocalVideoStop
+        {
+            get { return _isLocalVideoStop; }
+            set
+            {
+                _isLocalVideoStop = value;
+                OnPropertyChanged(nameof(IsLocalVideoStop));
+            }
+        }
+
+        public VideoActionsViewModel(Grid allContent)
+        {
+            _isConnected = true;
+
             _coreHandler = CoreHandler.Instance;
             _executeCloseWindowCommand = new CommandViewModel(CloseWindowAction);
+            _executeLoadVideoCommand = new CommandViewModel(LoadVideoAction);
+            _executePSLocalVideoCommand = new CommandViewModel(PSLocalVideo);
+
             _coreHandler.ConnectedStatusEvent += _coreHandler_ConnectedStatusEvent;
+            _coreHandler.ServerDisponibilityEvent += _coreHandler_ServerDisponibilityEvent;
+            _coreHandler.ClientsBeforeEvent += _coreHandler_ClientsBeforeEvent;
+            _coreHandler.VideoLoadedEvent += _coreHandler_VideoLoadedEvent;
+            _coreHandler.LocalVideoEvent += _coreHandler_LocalVideoEvent;
+            
+            _messagesPath = "Resources/Messages.xaml";
+            _messages = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, MessagesPath);
+
+            _allContent = allContent;
+            _defaultContent = allContent.Children.Cast<UIElement>().FirstOrDefault();
+            _defaultRowDefinitions = allContent.RowDefinitions.ToList();
+
+            HandleContentChange();
+        }
+
+        private void _coreHandler_LocalVideoEvent(object? sender, LocalVideoEventArgs e)
+        {
+            LocalVideo = e.LocalVideo;
+            ShowLocalVideo(LocalVideo);
+        }
+
+        private void ShowLocalVideo(byte[] videoBytes)
+        {
+            using (MemoryStream stream = new MemoryStream(videoBytes))
+            {
+                Uri videoUri = new Uri(stream.GetHashCode().ToString());
+                MediaElement mediaElementVideoLoaded = (MediaElement)Application.Current.MainWindow.FindName("mediaElementVideoLoaded");
+                mediaElementVideoLoaded.Source = videoUri;
+                mediaElementVideoLoaded.Stop();
+                IsLocalVideoStop = true;
+            }
+        }
+
+        private void _coreHandler_VideoLoadedEvent(object? sender, VideoLoadedEventArgs e)
+        {
+            IsVideoLoaded = e.IsVideoLoaded;
+
+            if (IsVideoLoaded)
+            {
+                Button btnPSbtnPSLoadVideo = (Button)Application.Current.MainWindow.FindName("btnPSLoadVideo");
+                btnPSbtnPSLoadVideo.Visibility = Visibility.Visible;
+                Button btnSendVideo = (Button)Application.Current.MainWindow.FindName("btnSendVideo");
+                btnSendVideo.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ChangeGridContent(UIElement newContent)
+        {
+            _allContent.Children.Clear();
+
+            if (newContent is TextBlock textBlock)
+            {
+                _allContent.RowDefinitions.Clear();
+                _allContent.RowDefinitions.Add(new RowDefinition());
+                Grid.SetRow(textBlock, 0);
+                _allContent.Children.Add(textBlock);
+            }
+            else
+            {
+                _allContent.RowDefinitions.Clear();
+                foreach (var rowDefinition in _defaultRowDefinitions)
+                {
+                    _allContent.RowDefinitions.Add(rowDefinition);
+                }
+                _allContent.Children.Add(newContent);
+            }
+        }
+
+        private void HandleContentChange()
+        {
+            ResourceDictionary resourceDic;
+            using (FileStream fs = new FileStream(Messages, FileMode.Open))
+            {
+                resourceDic = (ResourceDictionary)XamlReader.Load(fs);
+            }
+            TextBlock rMessage = (TextBlock)resourceDic["ResponseMessage"];
+            ChangeGridContent(rMessage);
         }
 
         private void _coreHandler_ConnectedStatusEvent(object? sender, ConnectedStatusEventArgs e)
         {
             IsConnected = e.Connected;
+        }
+
+        private void _coreHandler_ClientsBeforeEvent(object? sender, ClientsBeforeEventArgs e)
+        {
+            ClientsBefore = e.clientsBefore;
+        }
+
+        private void _coreHandler_ServerDisponibilityEvent(object? sender, ServerDisponibilityEventArgs e)
+        {
+            ServerDisponibility = e.serverDisponibility;
+
+            if (ServerDisponibility == Status.Waiting)
+            {
+                ResourceDictionary resourceDic;
+                using (FileStream fs = new FileStream(Messages, FileMode.Open))
+                {
+                    resourceDic = (ResourceDictionary)XamlReader.Load(fs);
+                }
+                TextBlock wMessage = (TextBlock)resourceDic["WaitingMessage"];
+                ChangeGridContent(wMessage);
+            } 
+            
+            else if (ServerDisponibility == Status.Busy)
+            {
+                ResourceDictionary resourceDic;
+                using (FileStream fs = new FileStream(Messages, FileMode.Open))
+                {
+                    resourceDic = (ResourceDictionary)XamlReader.Load(fs);
+                }
+                TextBlock bMessage = (TextBlock)resourceDic["BusyMessage"];
+                ChangeGridContent(bMessage);
+            } else if (ServerDisponibility == Status.Ready)
+            {
+
+            }
         }
 
         private void CloseWindowAction(object sender)
@@ -55,6 +256,27 @@ namespace Cluster_Client.ViewModel
                 CoreHandler.Instance.StopClientAsync();
             }
             Application.Current.Shutdown();
+        }
+
+        private void LoadVideoAction(object sender)
+        {
+            CoreHandler.Instance.LoadVideo();
+        }
+
+        private void PSLocalVideo(object sender)
+        {
+            MediaElement mediaElementVideoLoaded = (MediaElement)Application.Current.MainWindow.FindName("mediaElementVideoLoaded");
+
+            if (IsLocalVideoStop)
+            {
+                mediaElementVideoLoaded.Play();
+                IsLocalVideoStop = false;
+            }
+            else
+            {
+                mediaElementVideoLoaded.Stop();
+                IsLocalVideoStop = true;
+            }
         }
     }
 }
